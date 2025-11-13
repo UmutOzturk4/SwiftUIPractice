8a4088dd21380ad4efe5fc6d8621b48fe6c74486
Umut Öztürk <umutozmail1@gmail.com>
2025-11-13 15:40:05 +0300

    finished the intended project,leaving for now

---
+++ SwiftUIPractice/DetailsView.swift
@@ -0,0 +1,18 @@
+//
+//  Details.swift
+//  SwiftUIPractice
+//
+//  Created by Umut Öztürk on 13.11.2025.
+//
+
+import SwiftUI
+
+struct DetailsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DetailsView()
+}
---
+++ SwiftUIPractice/FavoriteModel.swift
@@ -0,0 +1,8 @@
+//
+//  FavoriteModel.swift
+//  SwiftUIPractice
+//
+//  Created by Umut Öztürk on 13.11.2025.
+//
+
+import Foundation
